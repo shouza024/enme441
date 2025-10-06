f637eac37077dca6fb6fda403d05009e4587624d
shouza024 <130184645+shouza024@users.noreply.github.com>
2025-10-06 18:12:19 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -4,13 +4,25 @@ import math
 
 GPIO.setmode(GPIO.BCM)
 
-pins = [17] #insert pin numbers once assigned.
+pins = [21] #insert pin numbers once assigned.
 base_frequency = 0.2 # frequency in hz
 pwm_frequency = 500 #hz
 
 for pin in pins:
     GPIO.setup(pin, GPIO.OUT)
+    print(f'Pin{pin} setup for ouput')
 
-pwm = GPIO.PWM(pins,f)
-duty_cycle = ((sin(2*pi()*base_frequency*time.time()))**2)*100
+pwm = GPIO.PWM(pins,pwm_frequency)
+try:
+    duty_cycle = ((sin(2*pi()*base_frequency*time.time()))**2)*100
+    pwm.start(duty_cycle)
+    while True:
+        pass
+except KeyboardInterrupt:
+    print('\nExiting')
+
+pwm.stop()
+pwm.cleanup()
+   
+
 
