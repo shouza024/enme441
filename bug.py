add57cfbed02405b66c8474779ab996af08ec143
shouza024 <130184645+shouza024@users.noreply.github.com>
2025-10-13 16:15:37 -0400

    Update bug.py

--- bug.py
+++ bug.py
@@ -8,11 +8,29 @@ import bugi as Bug
 GPIO.setmode(GPIO.BCM)
 
 dataPin, latchPin, clockPin = 23, 24, 25
-y = shifter.shifter(dataPin,latchPin,clockPin)
-bug = Bug.Bug(y,0.05,1,True)
+s1,s2,s3 = 5, 6, 13
 
-try:                             
-  bug.start()
+GPIO.setup(s1,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s2,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s3,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+y = shifter.shifter(dataPin,latchPin,clockPin)
+bug = Bug.Bug(y)
+
+def s2_call:
+    bug.isWrapon = not bug.isWrapon
+
+def s3_call:
+    bug.timestep = bug.timestep/3
+
+GPIO.add_event_detect(s2,GPIO.RISING,callback=s2_call,bouncetime=1000)
+GPIO.add_event_detect(s3,GPIO.RISING,callback=s3_call,bouncetime=1000)
+
+try:
+    while s1 = True:
+        bug.start()
+    else:
+        bug.stop()
 except KeyboardInterrupt:
   bug.stop()
   GPIO.cleanup()
