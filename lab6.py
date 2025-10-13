53e8d1e24e53ea71b1f4cc4ed670ef6cc927d5c3
shouza024 <130184645+shouza024@users.noreply.github.com>
2025-10-13 15:50:14 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -5,8 +5,8 @@ import random
 
 class Bug:
 
-   def  __init__(self,shifter,timestep = 0.1,x = 3,isWrapon = False):
-       self.shifter = shifter
+   def  __init__(self,__shifter,timestep = 0.1,x = 3,isWrapon = False):
+       self.__shifter = __shifter
        self.x = x
        self.timestep = timestep
        self.isWrapon = isWrapon
@@ -20,22 +20,27 @@ class Bug:
    def update(self,new_pos):
        self.pattern =(1<<new_pos)
 
-   def run(self):
-       self.shifter.shift_byte(self.pattern)
-       pos = self.finder()
-       if self.isWrapon == False:
-           if pos == 7:
-               new_pos = 6
-           elif pos == 0:
-               new_pos =1
-       else:
-           new_pos = pos + random.choice([-1,1])
-           if new_pos == 8:
-               new_pos = 1
-           elif new_pos ==0:
-               new_pos  = 7
-       self.update(new_pos)
-       time.sleep(self.timestep)
+   def start(self):
+       self.run = True
+       while self.run = True:
+           self.__shifter.shift_byte(self.pattern)
+           pos = self.finder()
+           if self.isWrapon == False:
+               if pos == 7:
+                   new_pos = 6
+               elif pos == 0:
+                   new_pos =1
+           else:
+               new_pos = pos + random.choice([-1,1])
+               if new_pos == 8:
+                   new_pos = 1
+               elif new_pos ==0:
+                   new_pos  = 7
+           self.update(new_pos)
+           time.sleep(self.timestep)
+
+    def stop(self):
+        self.run = False
 
 GPIO.setmode(GPIO.BCM)
 
@@ -44,7 +49,7 @@ y = shifter.shifter(dataPin,latchPin,clockPin)
 bug = Bug(y,0.05,1,True)
 
 try:                             
-  while 1:
-     bug.run()
+  bug.start()
 except KeyboardInterrupt:
+  bug.stop()
   GPIO.cleanup()
