252d944c294186bd1c454dda260aae97e43c55a6
shouza024 <130184645+shouza024@users.noreply.github.com>
2025-10-13 15:19:39 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -3,42 +3,41 @@ import time
 import shifter
 import random
 
+class Bug:
+
+   def  __init__(self,shifter,timestep = 0.1,x = 3,isWrapon = False):
+       self.x = x
+       self.pattern = (1 << self.x)
+      
+   def x(self):
+       for pos in range(8):
+           if self.pattern & (1 << pos):
+               return pos
+           
+    def update(self,new_pos):
+        self.pattern =(1<<new_pos)
+
+    def run(self):
+        self.shifter.shift_byte(pattern)
+        pos = self.x(pattern)
+        if pos == 7:
+            new_pos = 6
+        elif pos == 0:
+            new_pos =1
+        else:
+            new_pos = pos + random.choice([-1,1])
+                                  
+        self.update(new_pos)
+        time.sleep(self.timestep)
+
 GPIO.setmode(GPIO.BCM)
 
 dataPin, latchPin, clockPin = 23, 24, 25
 y = shifter.shifter(dataPin,latchPin,clockPin)
+bug = Bug(y,0.05)
 
-pattern = 0b01111110        # 8-bit pattern to display on LED bar
-
-start_pos=random.randint(0,7)#Choose a random starting positiong
-pattern = 0b00000000               #Initiate empty pattern
-
-pattern = (1 << start_pos)
-
-def x(pattern):
-   for pos in range(8):
-       if pattern & (1 << pos):
-           return pos
-           
-def update(new_pos):
-    global pattern 
-    pattern = 0b00000000
-    pattern =(1<<new_pos) 
-    
 try:                             
   while 1:
-      y.shift_byte(pattern)
-      pos = x(pattern)
-      if pos == 7:
-          new_pos = 6
-      elif pos == 0:
-          new_pos =1
-      else:
-          new_pos = pos + random.choice([-1,1])
-                                  
-      update(new_pos)
-      print(new_pos)
-      time.sleep(0.05)
-       
+     bug.run()
 except KeyboardInterrupt:
   GPIO.cleanup()
