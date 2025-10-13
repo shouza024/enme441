78b574f21b6265d61363fd5978f5204b95f46e79
shouza024 <130184645+shouza024@users.noreply.github.com>
2025-10-13 12:38:22 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,27 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
+class Shifter:
+    def __init__(self,serial_pin,latch_pin,clock_pin):
+        self.serial = serial_pin
+        self.clock = clock_pin
+        self.latch = latch_pin
+
+    def start():  
+        GPIO.setup(serial_pin, GPIO.OUT)
+        GPIO.setup(latch_pin, GPIO.OUT, initial=0)  
+        GPIO.setup(clock_pin, GPIO.OUT, initial=0)
+
+    def __ping(p):
+        GPIO.output(p,1)
+        time.sleep(0)
+        GPIO.output(p,0)
+      
+    def shift_byte(b): 
+        for i in range(8):
+            GPIO.output(dataPin, b & (1<<i))
+            ping(clockPin) 
+        ping(latchPin) 
+
 GPIO.setmode(GPIO.BCM)
 
 dataPin, latchPin, clockPin = 23, 24, 25
-
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)  
+x = Shifter(dataPin,latchPin,clockPin)
+x.start()
 
 pattern = 0b01100110        # 8-bit pattern to display on LED bar
 
-for i in range(8):
-  GPIO.output(dataPin, pattern & (1<<i))
-  GPIO.output(clockPin,1) 	    # ping the clock pin to shift register data
-  time.sleep(0)
-  GPIO.output(clockPin,0)
-
-GPIO.output(latchPin, 1)        # ping the latch pin to send register to output
-time.sleep(0)
-GPIO.output(latchPin, 0)
-
 try:
+  x.shift_byte(pattern)
   while 1: pass
 except:
   GPIO.cleanup()
