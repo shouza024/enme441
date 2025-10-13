104b114b55af3fb26ce80333732ca76882f449ff
shouza024 <130184645+shouza024@users.noreply.github.com>
2025-10-13 13:23:24 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,38 +1,42 @@
 import RPi.GPIO as GPIO
 import time
-
-class Shifter:
-    def __init__(self,serial_pin,latch_pin,clock_pin):
-        self.serial = serial_pin
-        self.clock = clock_pin
-        self.latch = latch_pin
-        self.start()
-
-    def start(self):  
-        GPIO.setup(self.serial, GPIO.OUT)
-        GPIO.setup(self.latch, GPIO.OUT, initial=0)  
-        GPIO.setup(self.clock, GPIO.OUT, initial=0)
-
-    def __ping(self,p):
-        GPIO.output(p,1)
-        time.sleep(0)
-        GPIO.output(p,0)
-      
-    def shift_byte(self,b): 
-        for i in range(8):
-            GPIO.output(dataPin, b & (1<<i))
-            self.__ping(clockPin) 
-        self.__ping(latchPin) 
+import shifter
+import random
 
 GPIO.setmode(GPIO.BCM)
 
 dataPin, latchPin, clockPin = 23, 24, 25
-x = Shifter(dataPin,latchPin,clockPin)
+y = shifter(dataPin,latchPin,clockPin)
 
 pattern = 0b01111110        # 8-bit pattern to display on LED bar
 
-try:
-  x.shift_byte(pattern)
+start_pos=random.randint(0,7)#Choose a random starting positiong
+pattern =[0]*8               #Initiate empty pattern
+
+pattern[start_pos] = 1
+
+def x(pattern):
+   for pos,val in enumerate(pattern):
+       if val == 1:
+           return pos
+           
+def update(new_pos):
+    pattern = [0]*8
+    pattern[new_pos] = 1
+    
+try:  
+  y.shift_byte(pattern)
+  x(pattern)
+  if x == 7;
+      new_pos = 6
+  else if x == 0:
+      new_pos =1
+  else:
+      new_pos = x + random.choice([-1,1]
+                                  
+  update(new_pos)
+  time.sleep(0.05)
+                                  
   while 1: pass
 except KeyboardInterrupt:
   GPIO.cleanup()
