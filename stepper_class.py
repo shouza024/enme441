1839da9819b50389b9970aa5f8a9b23bada23329
erick <erickcastillo20049@gmail.com>
2025-11-19 16:07:34 -0500

    1000

--- stepper_class.py
+++ stepper_class.py
@@ -25,7 +25,7 @@ class Stepper:
       # combined output bits for all motors
     seq = [0b0001, 0b0011, 0b0010, 0b0110,
            0b0100, 0b1100, 0b1000, 0b1001]  # CCW sequence
-    delay = 100             # delay between motor steps [us]
+    delay = 1000             # delay between motor steps [us]
     steps_per_degree = 4096 / 360.0  # 4096 steps per revolution
 
     # ===== Initialization =====
