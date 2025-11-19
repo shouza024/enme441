fad95b9e6bf094a74273e9f97d9755b7e6ab6fc8
erick <erickcastillo20049@gmail.com>
2025-11-19 18:21:15 -0500

    d

--- projectv1.py
+++ projectv1.py
@@ -372,11 +372,10 @@ s.listen(3)
 
 web_page_thread = threading.Thread(target=server_web_page)
 web_page_thread.daemon = True
-'''
+
 server_thread = threading.Thread(target=run_server)
 server_thread.daemon = True
 server_thread.start()
-'''
 web_page_thread.start()
 
 time.sleep(1)
