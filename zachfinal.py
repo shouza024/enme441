81a1dbfe75fadf2d9ab45d5033d83adba7f4c5ac
erick <erickcastillo20049@gmail.com>
2025-12-16 14:28:54 -0500

    change ip address

--- zachfinal.py
+++ zachfinal.py
@@ -142,7 +142,7 @@ data ={
 json_data = json.dumps(data)
 def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("127.0.1.1", 4084))   ##10.115.189.68
+    server.bind(("10.115.189.68", 4084))   ##10.115.189.68 Erick Ip ## 127.0.1.1
     server.listen(1)
 
     print("waiting for connection json server")
@@ -235,7 +235,7 @@ def go_next(target_coordinates,turret_coordinates):
     #return [turret_azimuth_angle, turret_altitude_angle]
 
 #-------------------Parsing Json-------------------------------
-url = "http://127.0.1.1:4084" #INSERT URL WHEN RELEASED "http://10.112.150.68:4084"
+url = "http://10.115.189.68:4084" #INSERT URL WHEN RELEASED "http://10.112.150.68:4084"
     #"http://192.168.1.254:8000/positions.json"
 def parse_json():
     
